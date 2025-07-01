50c1e24f554acb460b7f3ea9bd5aa571d3f34cc6
José Everton <vetophp@gmail.com>
2025-07-01 15:55:07 -0300

    Corrigido o nome da cidade

--- Atividade04.java
+++ Atividade04.java
@@ -3,6 +3,6 @@ public class Atividade04 {
         System.out.println("Meu nome é José");
         System.out.println("Minha idade é 20 anos");
         System.out.println("Meu curso é Engenharia de Software");
-        System.out.println("Parníba PI");
+        System.out.println("Parnaíba PI");
     }
 }
