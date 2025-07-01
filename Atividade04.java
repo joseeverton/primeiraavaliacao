b49ff4730e4aa86a78779da86f9cdb7a164fa74e
José Everton <vetophp@gmail.com>
2025-07-01 15:45:14 -0300

    Criando atividade 04

---
+++ Atividade04.java
@@ -0,0 +1,7 @@
+public class Atividade04 {
+    public static void main(String[] args) {
+        System.out.println("Meu nome é José");
+        System.out.println("Minha idade é 20 anos");
+        System.out.println("Meu curso é Engenharia de Software");
+    }
+}
